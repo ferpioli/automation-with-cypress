c6da07d068f6ef36bdb150bddbaaa234c7a2089c
fernando pimentel <ferpiolli@gmail.com>
2019-08-21 08:15:31 -0300

    interagindo com checkbox

--- cypress/integration/tickets.spec.js
+++ cypress/integration/tickets.spec.js
@@ -1,6 +1,6 @@
 describe('Tickets', () => {
     beforeEach(() => cy.visit("https://ticket-box.s3.eu-central-1.amazonaws.com/index.html"));
-    it("fills all the text input fields", ()=>{
+    it("fills all the text input fields", () => {
         const firstName = 'Fernando';
         const lastName = 'Oliveira';
         cy.get('#first-name').type(firstName);
@@ -9,12 +9,21 @@ describe('Tickets', () => {
         cy.get('#requests').type('vegetarian');
         cy.get('#signature').type(`${firstName} ${lastName}`);
     });
-    it.only('select two tickets', ()=> {
+    it('select two tickets', () => {
         cy.get('#ticket-quantity').select('2');
     });
-it.only('select vip ticket type', ()=>{
-    cy.get('#vip').check();
-});
+    it('select vip ticket type', () => {
+        cy.get('#vip').check();
+    });
+    it('selects social media checkbox', () =>{
+        cy.get('#social-media').check();
+    });
+    it.only('selcts friend and publication, then uncheck friend', ()=> {
+        cy.get('#friend').check();
+        cy.get('#publication').check();
+        cy.get('#friend').uncheck();
 
-    it("has 'TICKETBOX' header's heading", ()=> {});
+    });
+
+    it("has 'TICKETBOX' header's heading", () => { });
 });
